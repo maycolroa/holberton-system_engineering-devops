877efda94b2a36ffc0dae10578ed23ce32c21276
maycolroa <lehallo@hotmail.com>
2022-05-18 22:26:46 -0500

    0x15. API 0.01 18/05/22

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# using this REST API, for a given employee ID, returns information
+import requests
+import sys import argv
+
+def main():
+url = "https://jsonplaceholder.typicode.com"
